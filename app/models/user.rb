a43bbb540eae7ec771d8ec8b5cbdba60acf63bf2
nanarama <annasampers@gmail.com>
2022-10-14 15:38:03 -0500

    Add User to routes and controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :username, presence: true, uniqueness: true, length: { in: 3..25 }
+  validates :email, presence: true
+  validates :password, presence: true, length: { minimum: 8 }       
 end
