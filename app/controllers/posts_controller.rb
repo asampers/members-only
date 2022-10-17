7ee7cc00dc79b83fd0218519689bcef27c697e98
nanarama <annasampers@gmail.com>
2022-10-17 11:43:57 -0500

    Create simple form for new post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,12 @@
 class PostsController < ApplicationController
-  before_action :require_login, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
    #root "devise/sessions#new"
 
   resources :users
-  resources :posts, only [:new, :create]
+  resources :posts, only: [:new, :create, :index]
 end
