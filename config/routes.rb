1f00dd0c7658d90b508d90f69161d60ab85fd386
nanarama <annasampers@gmail.com>
2022-10-17 13:08:41 -0500

    Can successfully save a post, need to figure out logging out

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-   #root "devise/sessions#new"
+   root to: "posts#index"
 
-  resources :users
+  
   resources :posts, only: [:new, :create, :index]
 end
