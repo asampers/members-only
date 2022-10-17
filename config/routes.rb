421da46d9927b056cb113275ff838c50954c1565
nanarama <annasampers@gmail.com>
2022-10-17 15:26:28 -0500

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
-   root to: "posts#index"
+  root to: "posts#index"
 
-  
+  devise_for :users
   resources :posts, only: [:new, :create, :index]
 end
